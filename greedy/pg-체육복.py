11fe514c20c4c1331c142de71345c89f978749f1
HyeongKyu Park <hyeongkyu.park@bagelcode.com>
2023-10-13 00:54:13 +0900

    use in

--- greedy/pg-체육복.py
+++ greedy/pg-체육복.py
@@ -1,32 +1,18 @@
 def solution(n, lost, reserve):
     answer = 0
 
-    def existInLost(stu):
-        nonlocal lost
-        for i in range(len(lost)):
-            if stu == lost[i]:
-                return True
-        return False
-
-    def existInReserve(stu):
-        nonlocal reserve
-        for i in range(len(reserve)):
-            if stu == reserve[i]:
-                return True
-        return False
-
     for i in range(1, n + 1):
-        if existInLost(i) and existInReserve(i):
+        if i in lost and i in reserve:
             reserve.remove(i)
             lost.remove(i)
 
     for i in range(1, n + 1):
-        if not existInLost(i):
+        if i not in lost:
             answer += 1
-        elif existInLost(i) and (i - 1 <= n and existInReserve(i - 1)):
+        elif i in lost and (i - 1 <= n and i - 1 in reserve):
             answer += 1
             reserve.remove(i - 1)
-        elif existInLost(i) and (i + 1 >= 1 and existInReserve(i + 1)):
+        elif i in lost and (i + 1 >= 1 and i + 1 in reserve):
             answer += 1
             reserve.remove(i + 1)
     return answer
